dd3b814f6e2bd76c8d021e914f6f20787e18432d
BingRen <161671430+BingyBR@users.noreply.github.com>
2025-09-02 16:24:34 +0800

    standardise mobile dir

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -73,7 +73,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text, View } from 'react-native';
-import Profile from './screens/Profile';
+import Profile from './screens/profile';
 
 function HomeScreen() {
   return (
